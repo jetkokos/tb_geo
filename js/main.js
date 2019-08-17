4e3d9b521281a1dd5ab6c6bb513bb2bb2bce97b2
jetKoKoS <afeniks@bk.ru>
2019-08-17 13:12:40 +0300

    1st

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.part').hover (
+	function() {
+		$('.description').html($(this).attr('description-data'));
+		$('.description').fadeIn();
+	},
+	function() {
+		$('.description').fadeOut(50);
+	}
+)
