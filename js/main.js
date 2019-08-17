bb150814c30690c98855ddcfd056d95e8a86ae2e
jetKoKoS <afeniks@bk.ru>
2019-08-17 15:04:39 +0300

    mission 1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 $('.part').hover (
 	function() {
 		$('.description').html($(this).attr('description-data'));
-		$('.description').fadeIn();
+		$('.description').fadeIn(1);
 	},
 	function() {
-		$('.description').fadeOut(50);
+		$('.description').fadeOut(1);
+	}
+)
+
+$('.mission').hover (
+	function() {
+		$('.mission_text').html($(this).attr('mission_text-data'));
+		$('.mission_text').fadeIn(1);
+	},
+	function() {
+		$('.mission_text').fadeOut(1);
 	}
 )
