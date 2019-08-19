324093b3a80e3aae87ef1ecd12a5a129337c4bbe
jetKoKoS <afeniks@bk.ru>
2019-08-19 15:39:26 +0300

    vue

--- js/main.js
+++ js/main.js
@@ -1,49 +1,24 @@
-$('.part').hover (
-	function() {
-		$('.description').html($(this).attr('description-data'));
-		$('.description').fadeIn(1);
-	},
-	function() {
-		$('.description').fadeOut(1);
-	}
-)
+let app = new Vue({
+	el: '#app',
+	data: {
+		seen: false,
+		info: '',
+		
 
-$('.mission').hover (
-	function() {
-		$('.mission_text').html($(this).attr('mission-data'));
-		$('.mission_text').fadeIn(1);
 	},
-	function() {
-		$('.mission_text').fadeOut(1);
-	}
-)
+	methods: {
+		toggle() {
+			this.seen = !this.seen;
+		}
+		
+	  
+	   
+	},
 
-$('.mission_with_reqs').hover (
-	function() {
-		$('.mission_with_reqs_text').html($(this).attr('mission_with_reqs-data'));
-		$('.mission_with_reqs_text').fadeIn(1);
-	},
-	function() {
-		$('.mission_with_reqs_text').fadeOut(1);
+	computed: {
+		
+	   
 	}
-)
-
-$('.mission_special').hover (
-	function() {
-		$('.mission_special_text').html($(this).attr('mission_special-data'));
-		$('.mission_special_text').fadeIn(1);
-	},
-	function() {
-		$('.mission_special_text').fadeOut(1);
-	}
-)
-
-$('.squad').hover (
-	function() {
-		$('.squad_text').html($(this).attr('squad-data'));
-		$('.squad_text').fadeIn(1);
-	},
-	function() {
-		$('.squad_text').fadeOut(1);
-	}
-)
+	
+	
+});
