28d185e396fd53f14706a09d3d99e3720830f323
jetKoKoS <afeniks@bk.ru>
2019-08-20 15:42:33 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
-let app = new Vue({
-	el: '#app',
-	data: {
+Vue.component('mission', {
+	template: `
+		<div>
+			<img @click='seen = !seen' class='mission c1' src='media/mission_usual.svg' alt=''>
+			<div v-show='seen' class='info'>
+				<p class='h4'>Preferred combat mission teams</p>
+				<p>{{squads[0]}}</p>
+				<p>{{squads[1]}}</p>
+				<p>{{squads[2]}}</p>
+				<p>{{squads[3]}}</p>
+				<p class='h4'>Dangerous enemies</p>
+				<p>25% Mace (anti Jango)</p>
+				<p>25% Fives (anti Sion, NS, Wat Tambor)</p>
+			</div>
+		</div>
+	`,
+	data: function () {
+	  return {
 		seen: false,
 		squads: [
 			'Sith Empire',
@@ -12,25 +27,13 @@ let app = new Vue({
 			'Separatist Droids',
 			'Geonosians'			
 		],
-		fleets: [
-			'Chimera',
-		],
-		
-
-	},
-	methods: {
-		toggle() {
-			this.seen = !this.seen;
-		}
-		
-	  
-	   
-	},
-
-	computed: {
-		
-	   
+		missions: [
+			'c1',
+		]
+	  };
 	}
-	
-	
+});
+
+let app = new Vue({
+	el: '#app',
 });
