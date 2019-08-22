907dd8f821459cf60cee9d6ddd5c2ddfde5d9e24
jetKoKoS <afeniks@bk.ru>
2019-08-22 12:48:52 +0300

    "loop"

--- js/main.js
+++ js/main.js
@@ -1,41 +1,6 @@
 Vue.component('mission', {
 	template: `
-		<div>
-				<img src="media/mission_usual.svg">
-		</div>
-	`,
-	data: function () {
-	  return {
-	  };
-	}
-});
-Vue.component('mission_special', {
-	template: `
-		<div>
-			<img src="media/mission_special.svg">
-		</div>
-	`,
-	data: function () {
-	  return {
-	  };
-	}
-});
-Vue.component('mission_req', {
-	template: `
-		<div>
-			<img src="media/mission_req.svg">
-		</div>
-	`,
-	data: function () {
-	  return {
-	  };
-	}
-});
-Vue.component('squad_fill', {
-	template: `
-		<div>
-			<img src="media/squad.png">
-		</div>
+			<img class="mission">
 	`,
 	data: function () {
 	  return {
@@ -43,54 +8,50 @@ Vue.component('squad_fill', {
 	}
 });
 
+
 let app = new Vue({
 	el: '#app',
 	data: {
+		currentMission: '',
 		missions: [
-			{id: 0, name: 'c1'},
-			{id: 1, name: 'c3'},
-			{id: 2, name: 'c4'},
-			{id: 3, name: 'c5'},
-			{id: 4, name: 'c7'},
-			{id: 5, name: 'c9'},
-			{id: 6, name: 'c10'},
-			{id: 7, name: 'c11'},
-			{id: 8, name: 'c12'},
-			{id: 9, name: 'c14'},
-			{id: 10, name: 'c16'},
-			{id: 11, name: 'c17'},
-			{id: 12, name: 'c18'},
-			{id: 14, name: 'c19'},
-			{id: 15, name: 'c20'},
-			{id: 16, name: 'c22'},
-			{id: 17, name: 'c23'},
-		],
-		missions_req: [
-			{id: 0, name: 'c2'},
-			{id: 1, name: 'c6'},
-			{id: 2, name: 'c8'},
-			{id: 3, name: 'c13'},
-			{id: 4, name: 'c15'},
-			{id: 5, name: 'c21'},
-		],
-		missions_special: [
-			{id: 0, name: 's1'},
-			{id: 1, name: 's2'},
-			{id: 2, name: 's3'},
-			{id: 3, name: 's4'},
-		],
-		squad_fills: [
-			{id: 0, name: 't1'},
-			{id: 1, name: 'b1'},
-			{id: 2, name: 't2'},
-			{id: 3, name: 'm2'},
-			{id: 4, name: 'b2'},
-			{id: 5, name: 't3'},
-			{id: 6, name: 'm3'},
-			{id: 7, name: 'b3'},
-			{id: 8, name: 't4'},
-			{id: 9, name: 'm4'},
-			{id: 10, name: 'b4'},
+			{id: 0, name: 'c1', type: 'usual', path: 'media/mission_usual.svg'},
+			{id: 1, name: 'c2', type: 'req', path: 'media/mission_req.svg'},
+			{id: 2, name: 'c3', type: 'usual', path: 'media/mission_usual.svg'},
+			{id: 3, name: 'c4', type: 'usual', path: 'media/mission_usual.svg'},
+			{id: 4, name: 'c5', type: 'usual', path: 'media/mission_usual.svg'},
+			{id: 5, name: 'c6', type: 'req', path: 'media/mission_req.svg'}, 
+			{id: 6, name: 'c7', type: 'usual', path: 'media/mission_usual.svg'},
+			{id: 7, name: 'c8', type: 'req', path: 'media/mission_req.svg'},
+			{id: 8, name: 'c9', type: 'usual', path: 'media/mission_usual.svg'},
+			{id: 9, name: 'c10', type: 'usual', path: 'media/mission_usual.svg'},
+			{id: 10, name: 'c11', type: 'usual', path: 'media/mission_usual.svg'},
+			{id: 11, name: 'c12', type: 'usual', path: 'media/mission_usual.svg'},
+			{id: 12, name: 'c13', type: 'req', path: 'media/mission_req.svg'},
+			{id: 13, name: 'c14', type: 'usual', path: 'media/mission_usual.svg'},
+			{id: 14, name: 'c15', type: 'req', path: 'media/mission_req.svg'},
+			{id: 15, name: 'c16', type: 'usual', path: 'media/mission_usual.svg'},
+			{id: 16, name: 'c17', type: 'usual', path: 'media/mission_usual.svg'},
+			{id: 17, name: 'c18', type: 'usual', path: 'media/mission_usual.svg'},
+			{id: 18, name: 'c19', type: 'usual', path: 'media/mission_usual.svg'},
+			{id: 19, name: 'c20', type: 'usual', path: 'media/mission_usual.svg'},
+			{id: 20, name: 'c21', type: 'req', path: 'media/mission_req.svg'},
+			{id: 21, name: 'c22', type: 'usual', path: 'media/mission_usual.svg'},
+			{id: 22, name: 'c23', type: 'usual', path: 'media/mission_usual.svg'},
+			{id: 23, name: 's1', type: 'special', path: 'media/mission_special.svg'},
+			{id: 24, name: 's2', type: 'special', path: 'media/mission_special.svg'},
+			{id: 25, name: 's3', type: 'special', path: 'media/mission_special.svg'},
+			{id: 26, name: 's4', type: 'special', path: 'media/mission_special.svg'},
+			{id: 27, name: 't1', type: 'platoon', path: 'media/platoon.png'},
+			{id: 28, name: 'b1', type: 'platoon', path: 'media/platoon.png'},
+			{id: 29, name: 't2', type: 'platoon', path: 'media/platoon.png'},
+			{id: 30, name: 'm2', type: 'platoon', path: 'media/platoon.png'},
+			{id: 31, name: 'b2', type: 'platoon', path: 'media/platoon.png'},
+			{id: 32, name: 't3', type: 'platoon', path: 'media/platoon.png'},
+			{id: 33, name: 'm3', type: 'platoon', path: 'media/platoon.png'},
+			{id: 34, name: 'b3', type: 'platoon', path: 'media/platoon.png'},
+			{id: 35, name: 't4', type: 'platoon', path: 'media/platoon.png'},
+			{id: 36, name: 'm4', type: 'platoon', path: 'media/platoon.png'},
+			{id: 37, name: 'b4', type: 'platoon', path: 'media/platoon.png'}
 		],
 		squads: [
 			'Sith Empire',
@@ -102,10 +63,6 @@ let app = new Vue({
 			'Separatist Droids',
 			'Geonosians'			
 		],
-		images: [
-			'mission_usual.svg',
-			'mission_special.svg',
-			'mission_special.svg'
-		]
+		
 	}
 });
